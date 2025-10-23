43619382ecb52544fefb71d70dbb535ad97504a5
Mvalenzuelavml <miguel.valenzuela@vml.com>
2025-10-22 23:46:33 -0600

    Terminó la sección 4

--- src/shopping-cart/ItemCounter.tsx
+++ src/shopping-cart/ItemCounter.tsx
@@ -1,32 +1,44 @@
+import { useState } from "react";
+//import './ItemCounter.css'
+import styles from './ItemCounter.module.css'
 interface Props {
     nameProduct: string,
     quantity: number | undefined
 }
-export const ItemCounter = ({ nameProduct, quantity }: Props) => {
+export const ItemCounter = ({ nameProduct, quantity = 1 }: Props) => {
+
+    //Aquí van los hooks
+    //Esto es local del componente
+    const [count, setCount] = useState(quantity)
+    const handleAdd=()=>{
+         setCount(count+1);
+    }
+    const handleSubstract=()=>{
+        if(count===1) return;
+        setCount(count-1);
+        
+    }
 
     const handleClick = () => {
         console.log(`Click en ${nameProduct}`)
     }
+
+    
     return (
-        <section style={{
-            display: 'flex',
-            alignItems: 'center',
-            gap: 10,
-            marginTop: 10,
-        }}
-        >
-            <span style={{
-                width: 150,
+        <section className={styles.itemRow} >
+            <span className={ styles['item-text']} style={{
+                
+                color: count === 1 ? 'red' : 'black'
             }}
             >
                 {nameProduct}
             </span>
 
             <button
-                onClick={handleClick}
-            >+1</button>
-            <span>{quantity}</span>
-            <button>-1</button>
+                onClick={handleSubstract}
+            >-1</button>
+            <span>{count}</span>
+            <button  onClick={handleAdd}>+1</button>
         </section>
     )
 }
