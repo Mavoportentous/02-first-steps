38dfc21ae4b41d34441d33f33a7bc343fb672e5d
Miguel Valenzuela <ortizmav@gmail.com>
2025-10-27 13:07:14 -0600

    Vitest iniciado- Dell

--- src/shopping-cart/ItemCounter.tsx
+++ src/shopping-cart/ItemCounter.tsx
@@ -10,24 +10,24 @@ export const ItemCounter = ({ nameProduct, quantity = 1 }: Props) => {
     //Aquí van los hooks
     //Esto es local del componente
     const [count, setCount] = useState(quantity)
-    const handleAdd=()=>{
-         setCount(count+1);
+    const handleAdd = () => {
+        setCount(count + 1);
     }
-    const handleSubstract=()=>{
-        if(count===1) return;
-        setCount(count-1);
-        
+    const handleSubstract = () => {
+        if (count === 1) return;
+        setCount(count - 1);
+
     }
 
-    const handleClick = () => {
-        console.log(`Click en ${nameProduct}`)
-    }
+    // const handleClick = () => {
+    //     console.log(`Click en ${nameProduct}`)
+    // }
 
-    
+
     return (
         <section className={styles.itemRow} >
-            <span className={ styles['item-text']} style={{
-                
+            <span className={styles['item-text']} style={{
+
                 color: count === 1 ? 'red' : 'black'
             }}
             >
@@ -38,7 +38,7 @@ export const ItemCounter = ({ nameProduct, quantity = 1 }: Props) => {
                 onClick={handleSubstract}
             >-1</button>
             <span>{count}</span>
-            <button  onClick={handleAdd}>+1</button>
+            <button onClick={handleAdd}>+1</button>
         </section>
     )
 }
